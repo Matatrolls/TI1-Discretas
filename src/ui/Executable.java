c3edd050d2792ca952a3eb7e642138a9b0393ffd
Juan Camilo Corrales Osvath <juancamilocorra@gmail.com>
2023-10-03 19:47:29 -0500

    agregar implementación de tabla hash

--- src/ui/Executable.java
+++ src/ui/Executable.java
@@ -2,7 +2,7 @@ package ui;
 
 import java.util.Scanner;
 
-import model.Controller;
+import model.*;
 
 public class Executable {
 
@@ -20,8 +20,40 @@ public class Executable {
 	 */
 	public static void main(String[] args) {
 
-		Executable ejecutable = new Executable();
-		ejecutable.menu();
+		/*Executable ejecutable = new Executable();
+		ejecutable.menu();*/
+		IHashTable<ObjetoPrueba, ObjetoPrueba> hash = new HashTable<>(10);
+
+		ObjetoPrueba obj1 = new ObjetoPrueba("Luis", 22);
+		ObjetoPrueba obj2 = new ObjetoPrueba("San", 23);
+		ObjetoPrueba obj3 = new ObjetoPrueba("Gregorio", 24);
+		ObjetoPrueba obj4 = new ObjetoPrueba("Magno", 25);
+		ObjetoPrueba obj5 = new ObjetoPrueba("Juan", 26);
+		ObjetoPrueba obj6 = new ObjetoPrueba("camilo", 27);
+		ObjetoPrueba obj7 = new ObjetoPrueba("corrales", 28);
+		ObjetoPrueba obj8 = new ObjetoPrueba("osvath", 29);
+		ObjetoPrueba obj9 = new ObjetoPrueba("el", 30);
+		ObjetoPrueba obj10 = new ObjetoPrueba("profe", 31);
+
+
+		System.out.println(hash.add(obj1, obj1));
+		hash.add(obj2, obj2);
+		hash.add(obj3, obj3);
+		hash.add(obj4, obj4);
+		hash.add(obj5, obj5);
+		hash.add(obj6, obj6);
+		System.out.println(hash.add(obj7, obj7));
+		hash.add(obj8, obj8);
+		hash.add(obj9, obj9);
+		hash.add(obj10, obj10);
+		ObjetoPrueba obj11 = new ObjetoPrueba("Hola mundo, este es un nombre intencionalemnte muy largo", 31);
+		hash.add(obj11, obj11);
+		System.out.println("borrando con la key del elemento 11 (1ra vez): " + hash.delete(obj11));
+		System.out.println("borrando el elemento 11 por segunda vez : "+ hash.delete(obj11));
+		System.out.println("borrando el elemento 11 por tercera vez : "+ hash.delete(obj11));
+		System.out.println("borrando el elemento 8: " + hash.delete(obj8));
+		System.out.println("borrando el elemento 8 (2): " + hash.delete(obj8));
+
 
 	}
 //|||||||||||||||||||||||||||||||||||MENUS||||||||||||||||||||||||||||||
@@ -30,6 +62,7 @@ public class Executable {
 	 * This function displays a menu with options for managing users, products, and uses, and allows the
 	 * user to select an option or exit the menu.
 	 */
+	/*
 	public void menu() {
 		boolean indicator=false;
 	
@@ -72,8 +105,8 @@ public class Executable {
 				break;
 			}
 		}
-	}
-
+	}*/
+/*
     public void undo() {
         controller.undo();
     }
@@ -84,5 +117,5 @@ public class Executable {
 
     public void storeTaskAndReminders() {
         controller.storeTaskAndReminders();
-    }
+    }*/
 }
