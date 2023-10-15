0379a4a1d15e3fa02e1445ec35dbe4c66ff8b9d9
Matatrolls <pro.nhk90@gmail.com>
2023-10-15 17:31:17 -0500

    final reno

--- src/tests/Tests.java
+++ src/tests/Tests.java
@@ -1,5 +1,17 @@
 package tests;
+//import junit.framework.TestCase;
+import java.util.ArrayList;
 
-public class Tests {
+import dataStructures.*;
+public class Tests //extends TestCase 
+{
+
+    private HashTable tasksStore = new HashTable<>( 500);
+    private Queue taskQueue= new Queue<>();
+
+    private  ArrayList priorityTaskList = new ArrayList<>();
+
+    private MaxHeap priorityTaskQueue = new MaxHeap<>(priorityTaskList);
+
     
 }
